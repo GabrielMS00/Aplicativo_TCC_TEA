db00c61f99b0c74ba362867a33d1af5e47026f34
GabrielMS00 <gabriel.marquesdesouza.02@gmail.com>
2025-09-26 00:12:53 -0300

    refactor: refatoração do front-end da tela de login

--- app-tea/tailwind.config.js
+++ app-tea/tailwind.config.js
@@ -6,19 +6,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#5636D3',
-        secondary: '#FF872C',
-        secondary_light: 'rgba(255, 135, 44, 0.3)',
-        success: '#12A454',
-        success_light: 'rgba(18, 164, 84, 0.5)',
-        attention: '#E83F5B',
-        attention_light: 'rgba(232, 63, 91, 0.5)',
-        shape: '#FFFFFF',
-        title: '#363F5F',
-        text: '#969CB2',
-        background: '#F0F2F5',
-        dark_purple: '#26195C',
-        cherry: '#9C001A'
+        primary: '#87CFCF',
+        secondary: '',
+        text: '#2C3E50',
+        success: '#A6C98C',
+        attention: '#F16038',
+        background: '#F5F5F5'
       },
       borderWidth: {
         '1.5': '1.5px',
