464c47c44c82ed1536ef7dd64781fbc4a692c8db
GabrielMS00 <gabriel.marquesdesouza.02@gmail.com>
2025-10-04 17:17:16 -0300

    fix: correção dos inputs que estava ficando escondidos pelo teclado

--- app-tea/app/(auth)/CreateAccount.tsx
+++ app-tea/app/(auth)/CreateAccount.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { router } from 'expo-router';
 
 const LoginScreen = () => {
     const [nome, setNome] = useState('');
-    const [sobrenome, setSobrenome] = useState('');
+    const [cpf, setCpf] = useState('');
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
     const [confirmarSenha, setConfirmarSenha] = useState('');
@@ -18,43 +18,54 @@ const LoginScreen = () => {
     return (
         <View className='flex-1 bg-background p-5'>
 
-            <View className='flex-1 justify-center'>
+            <Text className='text-5xl font-extrabold text-primary text-center mb-14 pt-20'>Cadastro</Text>
 
-                <Text className='text-5xl font-extrabold text-primary text-center mb-14'>Cadastro</Text>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={{ flex: 1 }}
+            >
 
-                <View className='mb-7'>
-                    <Text className='text-xl font-semibold text-text mb-2'>Nome</Text>
-                    <Input value={nome} onChangeText={setNome} />
-                </View>
+                <ScrollView>
 
-                <View className='mb-7'>
-                    <Text className='text-xl font-semibold text-text mb-2'>Sobrenome</Text>
-                    <Input value={sobrenome} onChangeText={setSobrenome} />
-                </View>
+                    <View className='flex-1 justify-center'>
 
-                <View className='mb-7'>
-                    <Text className='text-xl font-semibold text-text mb-2'>E-mail</Text>
-                    <Input value={email} onChangeText={setEmail} />
-                </View>
+                        <View className='mb-5'>
+                            <Text className='text-xl font-semibold text-text mb-2'>Nome</Text>
+                            <Input value={nome} onChangeText={setNome} />
+                        </View>
 
-                <View className='mb-7'>
-                    <Text className='text-xl font-semibold text-text mb-2'>Senha</Text>
-                    <Input value={senha} onChangeText={setSenha} />
-                </View>
+                        <View className='mb-5'>
+                            <Text className='text-xl font-semibold text-text mb-2'>cpf</Text>
+                            <Input value={cpf} onChangeText={setCpf} keyboardType='numeric' />
+                        </View>
 
-                <View>
-                    <Text className='text-xl font-semibold text-text mb-2'>Confirmar Senha</Text>
-                    <Input value={confirmarSenha} onChangeText={setConfirmarSenha} />
-                </View>
+                        <View className='mb-5'>
+                            <Text className='text-xl font-semibold text-text mb-2'>E-mail</Text>
+                            <Input value={email} onChangeText={setEmail} />
+                        </View>
 
-            </View>
+                        <View className='mb-5'>
+                            <Text className='text-xl font-semibold text-text mb-2'>Senha</Text>
+                            <Input value={senha} onChangeText={setSenha} secureTextEntry />
+                        </View>
+
+                        <View>
+                            <Text className='text-xl font-semibold text-text mb-2'>Confirmar Senha</Text>
+                            <Input value={confirmarSenha} onChangeText={setConfirmarSenha} secureTextEntry />
+                        </View>
+
+                    </View>
+
+                </ScrollView>
+
+            </KeyboardAvoidingView>
 
             <View className='w-full flex-row justify-between'>
-                <Button title='Cancelar' onPress={handleCancelar}/>
+                <Button title='Cancelar' onPress={handleCancelar} />
                 <Button title='Confirmar' type='success' />
             </View>
 
-        </View>
+        </View >
     );
 };
 
