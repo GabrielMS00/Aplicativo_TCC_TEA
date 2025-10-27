ec027450c99413a8c91ec47b438d7255a43eb5b1
GabrielMS00 <gabriel.marquesdesouza.02@gmail.com>
2025-10-27 16:09:50 -0300

    fix: correção do checkbox da página de recriação da troca alimentar

--- app-tea/app/(tabs)/FoodExchange/FoodExchangeRemake.tsx
+++ app-tea/app/(tabs)/FoodExchange/FoodExchangeRemake.tsx
@@ -1,8 +1,45 @@
-import { View, Text, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, FlatList } from 'react-native';
 import { Button } from '../../../components/Button';
 import { FoodCardChecable } from '../../../components/FoodCardChecable';
 
+interface FoodItem {
+    id: string;
+    name: string;
+    isChecked: boolean;
+}
+
+const MOCKED_FOOD_SUGGESTIONS = [
+    { id: '1', name: 'Misto quente' },
+    { id: '2', name: 'Banana' },
+    { id: '3', name: 'Pão' },
+    { id: '4', name: 'Maçã Cozida' },
+    { id: '5', name: 'Iogurte' },
+];
+
 const Screen = () => {
+
+    const [foodList, setFoodList] = useState<FoodItem[]>(
+        MOCKED_FOOD_SUGGESTIONS.map(food => ({
+            ...food,
+            isChecked: false,
+        }))
+    );
+
+    const handleCheckChange = (idDoItemClicado: string, newValue: boolean) => {
+        setFoodList(currentFoodList =>
+            currentFoodList.map(food =>
+                food.id === idDoItemClicado
+                    ? { ...food, isChecked: newValue }
+                    : food
+            )
+        );
+    };
+
+    const handleProsseguir = () => {
+        const selectedFoods = foodList.filter(food => food.isChecked);
+        console.log('Alimentos selecionados:', selectedFoods.map(f => f.name));
+    };
 
     return (
         <View className='flex-1 bg-background p-5'>
@@ -11,16 +48,21 @@ const Screen = () => {
                 Selecione os alimentos aceitos
             </Text>
 
-            <ScrollView className='flex-1 mt-6'>
-                <View>
-                    {/* <FoodCardChecable food='Misto quente'/>
-                    <FoodCardChecable food='Banana'/>
-                    <FoodCardChecable food='Pão'/> */}
-                </View>
-            </ScrollView>
+            <FlatList
+                data={foodList}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <FoodCardChecable
+                        foodName={item.name}
+                        isChecked={item.isChecked}
+                        onValueChange={(newValue) => handleCheckChange(item.id, newValue)}
+                    />
+                )}
+                className='flex-1 mt-6'
+            />
 
             <View>
-                <Button title='Prosseguir' type='success' className='my-4' />
+                <Button title='Prosseguir' type='success' className='my-4' onPress={handleProsseguir} />
             </View>
 
         </View>
